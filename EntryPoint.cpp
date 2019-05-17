0f21eb89971ba457e23abc0c7f99bc0fea3e6124
padoleshital <45750512+padoleshital@users.noreply.github.com>
2019-05-17 14:19:52 +0530

    Add files via upload

---
+++ EntryPoint.cpp
@@ -0,0 +1,15 @@
+#include"Myheader.h"
+
+int main()
+{
+	int ivalue=0;
+	char ivalue1='ch';
+	printf("Enter number");
+	scanf("%d",&ivalue);
+	printf("Enter character");
+	scanf("%c",&ivalue1);
+
+	pattern_2(ivalue,'ch');
+	
+	return 0;
+}
---
+++ Helper.cpp
@@ -0,0 +1,11 @@
+#include"Myheader.h"
+
+void pattern_2(int ino,char ch)
+{
+	char icnt=0;
+	for(icnt=1;icnt<=ino;icnt++)
+	{
+		printf("%c\t",ch);
+
+	}
+}
